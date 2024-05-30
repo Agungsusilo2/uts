f0dbfbf78346800ad5c7578ffacbb0f65f7ab3d5
Agung <agungsusilo683@gmail.com>
2024-05-30 13:24:29 +0700

    add

--- src/main/java/uts/App.java
+++ src/main/java/uts/App.java
@@ -1,13 +1,25 @@
 package uts;
 
+import uts.repository.UserRepository;
+import uts.repository.UserRepositoryImpl;
+import uts.service.UserServiceImpl;
+import uts.util.DatabaseUtil;
+import uts.view.UserView;
+
+import javax.swing.*;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        UserRepository userRepository = new UserRepositoryImpl(DatabaseUtil.gDataSource());
+        UserServiceImpl userService = new UserServiceImpl(userRepository);
+        SwingUtilities.invokeLater(() -> {
+            UserView userView = new UserView(userService);
+            userView.setVisible(true);
+        });
     }
 }
